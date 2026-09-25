7b4fd178f0a677b8368e3c85eae3c9ffeb0895f4
isabellapiras <isabella.piras@gmail.com>
2026-09-25 14:21:13 +0200

    Icone social del footer generate con map

--- src/Components/App/Footer.jsx
+++ src/Components/App/Footer.jsx
@@ -1,3 +1,11 @@
+const socials = [
+    { id: 1, name: 'Facebook', icon: '/img/footer-facebook.png', href: '#' },
+    { id: 2, name: 'Twitter', icon: '/img/footer-twitter.png', href: '#' },
+    { id: 3, name: 'YouTube', icon: '/img/footer-youtube.png', href: '#' },
+    { id: 4, name: 'Pinterest', icon: '/img/footer-pinterest.png', href: '#' },
+    { id: 5, name: 'Periscope', icon: '/img/footer-periscope.png', href: '#' },
+];
+
 export default function Footer() {
     return (
         <footer className="bg-neutral-800">
@@ -11,11 +19,11 @@ export default function Footer() {
                     <span className="text-blue-600 text-xl font-bold uppercase mr-4">
                         Follow us
                     </span>
-                    <img src="/img/footer-facebook.png" alt="Facebook" />
-                    <img src="/img/footer-twitter.png" alt="Twitter" />
-                    <img src="/img/footer-youtube.png" alt="YouTube" />
-                    <img src="/img/footer-pinterest.png" alt="Pinterest" />
-                    <img src="/img/footer-periscope.png" alt="Periscope" />
+                    {socials.map((social) => (
+                        <a key={social.id} href={social.href}>
+                            <img src={social.icon} alt={social.name} />
+                        </a>
+                    ))}
                 </div>
 
             </div>
